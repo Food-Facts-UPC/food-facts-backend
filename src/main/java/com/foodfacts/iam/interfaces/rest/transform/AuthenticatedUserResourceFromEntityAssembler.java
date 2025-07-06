07b90805222330621467704e6374e96d824b0d89
Diego22rct <diegorafaelcisnerostafur@gmail.com>
2025-07-06 03:57:51 -0500

    feat: Include roles in AuthenticatedUserResource and update assembler to populate roles

--- src/main/java/com/foodfacts/iam/interfaces/rest/transform/AuthenticatedUserResourceFromEntityAssembler.java
+++ src/main/java/com/foodfacts/iam/interfaces/rest/transform/AuthenticatedUserResourceFromEntityAssembler.java
@@ -3,8 +3,14 @@ package com.foodfacts.iam.interfaces.rest.transform;
 import com.foodfacts.iam.domain.model.aggregates.User;
 import com.foodfacts.iam.interfaces.rest.resources.AuthenticatedUserResource;
 
+import java.util.List;
+import java.util.stream.Collectors;
+
 public class AuthenticatedUserResourceFromEntityAssembler {
     public static AuthenticatedUserResource toResourceFromEntity(User user, String token) {
-        return new AuthenticatedUserResource(user.getId(), user.getUsername(), token);
+        List<String> roles = user.getRoles().stream()
+                .map(role -> role.getName().name())
+                .collect(Collectors.toList());
+        return new AuthenticatedUserResource(user.getId(), user.getUsername(), token, roles);
     }
 }
